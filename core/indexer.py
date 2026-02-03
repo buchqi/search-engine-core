72a94ccb6b94ff743492174f4f31ba4f16b9d853
buchqi <giorgitkebuchava15@gmail.com>
2026-02-04 01:00:38 +0400

    core ranking functionality

--- core/indexer.py
+++ core/indexer.py
@@ -9,7 +9,7 @@ class Indexer:
         #Term frecuencies doc_id:{word:count}
         self.term_freqs = {}
         
-    def add_document(self,doc_id:int,tokens: list[str]):
+    def _add_document(self,doc_id:int,tokens: list[str]):
         """Add document to the index"""
         #update inverted_index for each toke
         
@@ -26,7 +26,27 @@ class Indexer:
                 self.term_freqs[doc_id][token] +=1
             else:
                 self.term_freqs[doc_id][token] = 1
-    def get_docs(self,term:str):
-        
+    def _get_docs(self,term:str):
+        """it should return set of document ids where term word is found"""
         return self.inverted_index.get(term,set())
+    def _rank_docs(self,doc_id:set[int],tokens:set[str]):
+        """it should return ranked list of documents,after getting do_id's where list of tokens appeared"""
+        ranking = []
+        for ID in doc_id:
+            score = 0
+            for token in tokens:
+                score += self.term_freqs[ID].get(token,0)
+            if score >0:
+                ranking.append({"doc_id":ID,"score":score})
+        #sort by descending order 
+        ranking.sort(key = lambda x: -x["score"])                 
+        return ranking
         
+    
+    def search(query:str,mode = "AND"):
+       pass
+        
+indexer = Indexer()
+indexer._add_document(1,['apple','orange','python',])
+indexer._add_document(2,['orange','python','apple','apple'])
+indexer._rank_docs({1,2}, {'apple'})
